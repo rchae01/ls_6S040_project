c3d5fd6fac210b8561b286ac82a78c5743b488bf
rchae01 <68734585+rchae01@users.noreply.github.com>
2022-12-03 23:29:52 -0500

    small edit5

--- ls/datasets/__init__.py
+++ ls/datasets/__init__.py
@@ -1,6 +1,8 @@
 from .NoisyMNIST import NoisyMNIST
 from .Tox21 import Tox21
+from .Tox21g import Tox21g
 from .CelebA import CelebA
 from .Waterbirds import Waterbirds
 from .MNLI_bert_uncased import MNLI_bert_uncased
 from .BeerReviews import BeerReviews
+

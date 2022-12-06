72cac977792fbdffe5a17a41735e3d1eefdab607
rchae01 <68734585+rchae01@users.noreply.github.com>
2022-12-05 19:39:00 -0500

    small edit5

--- ls/models/GNN.py
+++ ls/models/GNN.py
@@ -66,8 +66,8 @@ class BasicGNN(torch.nn.Module):
     """
     def __init__(
         self,
-        in_channels: int = 14,
-        hidden_channels: int = 14,
+        in_channels: int = -1,
+        hidden_channels: int = 9,
         num_layers: int = 9,
         out_channels: Optional[int] = None,
         dropout: float = 0.0,

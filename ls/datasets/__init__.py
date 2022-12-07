a2884ce6d98b51a8ddcb275224e35cd2a88effae
rchae01 <68734585+rchae01@users.noreply.github.com>
2022-12-07 13:18:40 -0500

    smiles

--- ls/datasets/__init__.py
+++ ls/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .NoisyMNIST import NoisyMNIST
 from .Tox21 import Tox21
 from .Tox21g import Tox21g
+from .Tox21g_smiles import Tox21g_smiles
 from .CelebA import CelebA
 from .Waterbirds import Waterbirds
 from .MNLI_bert_uncased import MNLI_bert_uncased
